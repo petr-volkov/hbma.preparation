545e0e1ebb4642d95dc8ee986cd538d12989edb3
Petr Volkov <petr.volkov@med.lu.se>
2016-03-14 22:03:22 +0100

    Added writer monad to the reading process

--- src/ReadFiles.hs
+++ src/ReadFiles.hs
@@ -1,31 +1,43 @@
 module ReadFiles where
 import System.Directory
 import System.FilePath.Posix
+import Control.Monad.Writer
+import Debug.Trace
 
 --- filter only .fa files (for testing purposes, take only 3 for now)
-filterFastaFiles :: [FilePath] -> [FilePath]
-filterFastaFiles files = take 3 [file | file <- files, takeExtension file == ".fa"]
+filterFastaFiles :: [FilePath] -> Writer [String] [FilePath]
+filterFastaFiles files = writer (fastaFiles,
+                                  ["Reading " ++ (show $ length fastaFiles) ++ " fasta files."])
+  where
+    fastaFiles = take 3 [file | file <- files, takeExtension file == ".fa"]
 
-appendGenomeDir :: String -> [FilePath] -> [FilePath]
-appendGenomeDir dir files = map (\file -> joinPath [dir, file]) files
-
---- Probably don't need this function at all, because outside of IO
---- it can only check that there are files in the list
-verifyFilesList :: [FilePath] -> Bool
-verifyFilesList [] = False
-verifyFilesList _ = True
+--- append dir path to fasta files, so there is a full path
+appendGenomeDir :: String -> [FilePath] -> Writer [String] [FilePath]
+appendGenomeDir dir files = writer (appendedFiles,
+                                    ["Dir path: " ++ dir])
+  where
+    appendedFiles = map (\file -> joinPath [dir, file]) files
 
 -- read directory
-readGenomeDir :: String -> IO [String]
+readGenomeDir :: String -> IO (Writer [String] [FilePath])
 readGenomeDir dir = do
   files <- getDirectoryContents dir
-  let filteredFiles = appendGenomeDir dir (filterFastaFiles files)
-  return filteredFiles
+  return $ (return files) >>= filterFastaFiles >>= (appendGenomeDir dir)
 
--- read files
 readFiles :: String -> IO ()
 readFiles dir = do
-  files <- readGenomeDir dir
-  contents <- mapM readFile files
-  mapM_ (putStrLn . (take 100)) contents
-  return ()
+    files <- runWriterT $ readGenomeDir' dir
+    mapM_ putStrLn (snd $ files)
+    contents <- mapM readFile (fst $ files)
+    mapM_ (putStrLn . (take 100)) contents
+    return ()
+
+liftWriter :: Writer [String] [FilePath] -> WriterT [String] IO ([FilePath])
+liftWriter = WriterT . return . runWriter
+
+readGenomeDir' :: String -> WriterT [String] IO ([FilePath])
+readGenomeDir' dir = do
+    files <- liftIO $ getDirectoryContents dir
+    files2 <- liftWriter $ filterFastaFiles files
+    files3 <- liftWriter $ appendGenomeDir dir files2
+    return files3
