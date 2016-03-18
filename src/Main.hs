bf2fcf8468912f50595f48ae13831e85fd84f1e6
Petr Volkov <petr.volkov@med.lu.se>
2016-03-18 16:11:33 +0100

    Modified monad transformer stack for reading files

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,11 @@
+import Control.Monad.Trans.Maybe
 import CommandLine
 import ReadFiles
 
 run :: Params -> IO ()
 run params = do
-  readFiles $ genome_path params
-  return ()  
+  readFiles $ genome_path params  
+  return ()
 
 main :: IO ()
 main = showHelpOnErrorExecParser params >>= run
--- src/ReadFiles.hs
+++ src/ReadFiles.hs
@@ -1,43 +1,48 @@
 module ReadFiles where
+import Control.Monad.Identity
 import System.Directory
 import System.FilePath.Posix
+import Control.Monad.Trans.Maybe
 import Control.Monad.Writer
 import Debug.Trace
 
---- filter only .fa files (for testing purposes, take only 3 for now)
-filterFastaFiles :: [FilePath] -> Writer [String] [FilePath]
-filterFastaFiles files = writer (fastaFiles,
-                                  ["Reading " ++ (show $ length fastaFiles) ++ " fasta files."])
-  where
-    fastaFiles = take 3 [file | file <- files, takeExtension file == ".fa"]
+-- type IMWriter a = (WriterT [String]) (MaybeT IO) a
+type FastaReaderT a = MaybeT (WriterT [String] IO) a
 
---- append dir path to fasta files, so there is a full path
-appendGenomeDir :: String -> [FilePath] -> Writer [String] [FilePath]
-appendGenomeDir dir files = writer (appendedFiles,
-                                    ["Dir path: " ++ dir])
+liftMaybe :: Maybe a -> FastaReaderT a
+liftMaybe a = (MaybeT . lift . return) a
+
+-- append dir path to fasta files, so there is a full path
+appendGenomeDir :: String -> [FilePath] -> [FilePath]
+appendGenomeDir dir files = appendedFiles
   where
     appendedFiles = map (\file -> joinPath [dir, file]) files
+      
+filterFastaFiles :: [FilePath] -> Maybe [FilePath]
+filterFastaFiles files
+  | filtered == [] = Nothing
+  | otherwise = Just filtered
+  where 
+    filtered = take 3 [file | file <- files, takeExtension file == ".fa"]
 
--- read directory
-readGenomeDir :: String -> IO (Writer [String] [FilePath])
+readGenomeDir :: String -> FastaReaderT [FilePath]
 readGenomeDir dir = do
-  files <- getDirectoryContents dir
-  return $ (return files) >>= filterFastaFiles >>= (appendGenomeDir dir)
+  tell ["Reading files"]
+  allFiles <- liftIO $ getDirectoryContents dir
+  tell ["Selecting fasta files"]
+  fastaFiles <- liftMaybe $ filterFastaFiles allFiles
+  tell ["Appending genome dir"]
+  return $ appendGenomeDir dir fastaFiles  
 
+-- for testing purposes this function retuns IO() but it will change
 readFiles :: String -> IO ()
 readFiles dir = do
-    files <- runWriterT $ readGenomeDir' dir
-    mapM_ putStrLn (snd $ files)
-    contents <- mapM readFile (fst $ files)
-    mapM_ (putStrLn . (take 100)) contents
-    return ()
+  res <- runWriterT $ runMaybeT $ readGenomeDir dir
+  putStrLn "Files:"
+  putStrLn $ show $ fmap (\xs -> take 3 xs) (fst res)
+  putStrLn "Log:"
+  putStrLn $ show $ snd res
+  return ()
 
 liftWriter :: Writer [String] [FilePath] -> WriterT [String] IO ([FilePath])
 liftWriter = WriterT . return . runWriter
-
-readGenomeDir' :: String -> WriterT [String] IO ([FilePath])
-readGenomeDir' dir = do
-    files <- liftIO $ getDirectoryContents dir
-    files2 <- liftWriter $ filterFastaFiles files
-    files3 <- liftWriter $ appendGenomeDir dir files2
-    return files3
